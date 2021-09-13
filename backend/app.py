0c5a1178934e934fa8e3a91900628dff5824bf46
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2021-09-13 14:17:20 +0900

    fix

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def top():
+def hello():
     name = "test"
     return name + "\n"
 
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=80) 
+    app.run(debug=True, host='localhost', port=8000)
