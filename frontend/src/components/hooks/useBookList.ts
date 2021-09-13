d681ef3b139cd65df8bcbfd844d6cc2abae3ebf6
SUDA Mikihrio <s@sudame.net>
2021-09-13 17:49:45 +0900

    add: #39 開発環境と本番環境でAPIを叩く関数を使い分ける

--- frontend/src/components/hooks/useBookList.ts
+++ frontend/src/components/hooks/useBookList.ts
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
-import { __mockGetBookList } from "../../api/BookAPI";
+import { getBookList } from "../../api";
 import { BookListItem } from "../../models/BookListItem";
 
 export const useBookList = () => {
   const [bookList, setBookList] = useState<BookListItem[]>([]);
 
   const _getBookList = async () => {
-    setBookList(await __mockGetBookList("1"));
+    setBookList(await getBookList("1"));
   };
 
   useEffect(() => {
