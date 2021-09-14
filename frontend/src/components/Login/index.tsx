70435f509094ab85ac1569d29e9881dbb65bfcc7
xiao0232 <t409828543087@gmail.com>
2021-09-14 18:00:24 +0900

    add: #69 Loginのstyle設定

--- frontend/src/components/Login/index.tsx
+++ frontend/src/components/Login/index.tsx
@@ -14,13 +14,23 @@ export const Login: React.FC = () => {
 
   return (
     <>
-      <div className="login-container">
-        <div className="page-title">
+      <div className="login-container login-center">
+        <div className="login-header">
           <p>ログイン</p>
         </div>
-        <form onSubmit={handleSubmit}>
-          <input type="text" onChange={handleInputChange} />
-          <input type="submit" value="ログイン" />
+        <form className="login-form login-center" onSubmit={handleSubmit}>
+          <label className="login-form-label">ユーザーID</label>
+          <input
+            className="login-form-input login-form-text"
+            type="text"
+            placeholder=""
+            onChange={handleInputChange}
+          />
+          <input
+            className="login-form-input login-form-submit"
+            type="submit"
+            value="ログイン"
+          />
         </form>
       </div>
     </>
