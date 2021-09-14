e5b2bd0197a35c41be7e0c6d1de478d4102cbc54
SUDA Mikihrio <s@sudame.net>
2021-09-14 10:51:33 +0900

    update: パネルとモーダルを分離

--- frontend/src/components/BookInfoModal/index.tsx
+++ frontend/src/components/BookInfoModal/index.tsx
@@ -1,4 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getBookByID } from "../../api/BookAPI";
+import { Book } from "../../models/Book";
+
+type PanelProp = {
+  book: Book;
+};
+
+const BookInfoModalPanel: React.FC<PanelProp> = ({ book }) => {
+  return (
+    <div className="book-info-panel">
+      <div className="book-info-panel--title">{book.title}</div>
+      <div className="book-info-panel--image">
+        {book.image ? <img src={book.image} alt="本の表紙" /> : null}
+      </div>
+      <div className="book-info-panel--title">{book.author}</div>
+      <div className="book-info-panel--title">{book.description}</div>
+    </div>
+  );
+};
 
 type Prop = {
   bookId: string;
@@ -6,13 +25,27 @@ type Prop = {
 };
 
 export const BookInfoModal: React.FC<Prop> = ({ bookId, onCloseModal }) => {
+  const [bookInfo, setBookInfo] = useState<Book>();
+
+  const _getBookInfo = async (bookId: string) => {
+    setBookInfo(await getBookByID("1", bookId));
+  };
+
+  useEffect(() => {
+    _getBookInfo(bookId);
+  }, [bookId]);
+
   return (
     <div className="book-info-modal-wrapper">
       <div className="book-info-modal">
         <div className="book-info-modal--close" onClick={onCloseModal}>
           X
         </div>
-        {bookId}
+        {bookInfo ? (
+          <BookInfoModalPanel book={bookInfo} />
+        ) : (
+          <div>Loading...</div>
+        )}
       </div>
     </div>
   );
