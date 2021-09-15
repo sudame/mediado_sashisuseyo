4d1d0cc7902ab2e00c7b4586b032e046639c4204
SUDA Mikihrio <s@sudame.net>
2021-09-15 11:27:54 +0900

    fix: ログインしていないときには本棚登録ボタンを出さない

--- frontend/src/components/AppBar/index.tsx
+++ frontend/src/components/AppBar/index.tsx
@@ -2,22 +2,27 @@ import React from "react";
 import "./style.scss";
 import logo from "./logo.svg";
 import titleLogo from "./logo_title.svg";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 export const AppBar: React.FC = () => {
+  const { userId } = useAuthContext();
+
   return (
     <div className="app-bar">
       <img className="app-bar--logo" src={logo} alt="" />
       <img className="app-bar--title-logo" src={titleLogo} alt="" />
       <div className="spacer"></div>
-      <button
-        className="app-bar--books-register"
-        onClick={() => {
-          // TODO: クリックしたときの挙動をどうにかする
-          window.alert("本棚連携の実装");
-        }}
-      >
-        本棚連携
-      </button>
+      {userId ? (
+        <button
+          className="app-bar--books-register"
+          onClick={() => {
+            // TODO: クリックしたときの挙動をどうにかする
+            window.alert("本棚連携の実装");
+          }}
+        >
+          本棚連携
+        </button>
+      ) : null}
     </div>
   );
 };
